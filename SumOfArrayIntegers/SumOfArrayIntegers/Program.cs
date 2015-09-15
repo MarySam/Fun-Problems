928d2ae15b5df57bea522b852777c45fd4b03438
Mary Sam <marysam01@hotmail.com>
2015-09-15 00:26:33 -0700

    Building a Calculator

---
+++ SumOfArrayIntegers/SumOfArrayIntegers/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfArrayIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+
+
+            Console.WriteLine("What numbers would you like to add?\nPlease separate each number with a comma");
+            string UserValue = Console.ReadLine();
+
+            Console.WriteLine("You typed " + UserValue);
+            Console.ReadLine();
+
+            string[] delimiterString = { "," };
+
+            string[] input = UserValue.Split(delimiterString, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("{0} numbers were typed in:", input.Length);
+
+            int sum = 0;
+            foreach (string valueTyped in input)
+            {
+                int conversion = int.Parse(valueTyped);
+
+                int[] arr = new int[] { conversion };
+                
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];
+                }
+
+                Console.WriteLine(sum);
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
