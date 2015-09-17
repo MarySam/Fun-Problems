52169487b24186c718ae650e5239112f21d858ac
Mary Sam <marysam01@hotmail.com>
2015-09-17 00:33:23 -0700

    whileLoop, forLoop, forEach loop.

--- SumOfArrayIntegers/SumOfArrayIntegers/Program.cs
+++ SumOfArrayIntegers/SumOfArrayIntegers/Program.cs
@@ -10,11 +10,7 @@ namespace SumOfArrayIntegers
     {
         static void Main(string[] args)
 
-
-
         {
-
-
             Console.WriteLine("What numbers would you like to add?\nPlease separate each number with a comma");
             string UserValue = Console.ReadLine();
 
@@ -26,23 +22,44 @@ namespace SumOfArrayIntegers
             string[] input = UserValue.Split(delimiterString, StringSplitOptions.RemoveEmptyEntries);
             Console.WriteLine("{0} numbers were typed in:", input.Length);
 
+            // While Loop
+            //int sum = 0;
+            //int i = 0;
+            //while (i < input.Length)
+            //{
+            //    string valueTyped = input[i];
+
+            //    int conversion = int.Parse(valueTyped);
+            //    sum += conversion;
+
+            //    i++;
+            //}
+
+            // For Loop
+            //int sum = 0;
+            //for (int x = 0; x < input.Length; x++)
+            //{
+            //    string valueTyped = input[x];
+
+            //    int conversion = int.Parse(valueTyped);
+            //    sum += conversion;
+            //}
+
+            // Foreach Loop
             int sum = 0;
             foreach (string valueTyped in input)
             {
                 int conversion = int.Parse(valueTyped);
+                sum += conversion;
 
-                int[] arr = new int[] { conversion };
-                
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    sum += arr[i];
-                }
-
-                Console.WriteLine(sum);
-                Console.ReadLine();
-
+                ///ShortCut: sum+=int.Parse(valueTyped); 
             }
 
+            Console.WriteLine("Your sum is " + sum);
+            Console.ReadLine();
+
         }
+
     }
+
 }
