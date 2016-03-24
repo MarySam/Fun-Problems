6073f4903ec4096e2b289d418226a9a26f11f243
Mary Sam <marysam01@hotmail.com>
2016-03-23 22:19:53 -0700

    RemoveSpacesInAStringInPlace

---
+++ RemoveSpacesofAStringInOnePass/RemoveSpacesInAStringInPlace/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveSpacesInAStringInPlace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] mySentence = { 'I', ' ', 'l', 'o', 'v', 'e', ' ', 'i', 'c', 'e', ' ', 'c', 'r', 'e', 'a', 'm', '.', };
+            foreach (char c in mySentence)
+            {
+                Console.Write("{0}", c);
+            }
+
+            char[] updatedSentence = RemoveSpaces(mySentence);
+            foreach (char c in updatedSentence)
+            {
+                Console.Write("{0}", c);
+            }
+            Console.ReadLine();
+        }
+
+        public static char[] RemoveSpaces(char[] mySentence)
+        {
+            int count = 0;
+
+            for (int i = 0; i <= mySentence.Length - 1; i++)
+            {
+                if (mySentence[i] != ' ')
+                {
+                    mySentence[count] = mySentence[i];
+                    count++;
+                }
+                else
+                {
+                    mySentence[count] = '\0';
+                }
+            }
+
+            for (int i = count; i <= mySentence.Length-1; i++)
+            {
+                mySentence[i] = '\0';
+            }
+
+            return mySentence;
+        }
+    }
+}
+
+
