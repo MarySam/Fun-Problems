98b2fe1af348b4c3aaf6d1a5f89acd2e9b1434f3
Mary Sam <marysam01@hotmail.com>
2016-04-02 00:18:19 -0700

    RotatedArray

---
+++ ReverseAString/ReverseAString/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] myArray = new char[] { 'I', ' ', 'l', 'o', 'v', 'e', ' ', 'i', 'c', 'e', ' ', 'c', 'r', 'e', 'a', 'm', '.', };
+            //Before
+            //foreach (char c in myArray)
+            //{
+            //    Console.Write("{0}", c);
+            //}
+
+            //Reversing In Place.
+            //ReverseChar(myArray);
+
+            //Reversing Using a Stack.
+            //ReverseInStack(myArray);
+
+            foreach (char c in myArray)
+            {
+                Console.Write("{0}", c);
+            }
+
+            Console.ReadLine();
+        }
+
+        //Reverse every character in place.
+        //O(n)
+        public static char[] ReverseChar(char[] myArray)
+        {
+            int start = 0;
+            int end = myArray.Length-1;
+            char temp;
+                while (start < end)
+                {
+                    temp = myArray[start];
+                    myArray[start] = myArray[end];
+                    myArray[end] = temp;
+                    start++;
+                    end--;
+                }
+            return myArray;
+        }
+
+        public static char[] ReverseEachWord (char start)
+        {
+            char s;
+            char temp;
+
+           if(temp == )
+
+
+
+        }
+
+        public static void ReverseAWord(char start, char end)
+        {
+            char temp;
+            while (start<end)
+            {
+                temp = start;
+                start = end;
+                end = temp;
+                start++;
+                end--;
+            }
+        }
+
+        //Reverse every character using a stack.
+        //O(2n)? plus addnl data structure(stack).
+        public static char[] ReverseInStack(char[] myArray)
+        {
+            Stack<char> myStack = new Stack<char>();
+
+            foreach (char c in myArray)
+            {
+                myStack.Push(c);
+            }
+
+            for (int i = 0; i <= myArray.Length - 1; i++)
+            {
+                char Popped = myStack.Pop();
+                myArray[i] = Popped;
+            }
+
+            return myArray;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ RotatedArray/RotatedArray/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotatedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = new int[] { 4, 5, 6, 7, 8, 9, 10, 1, 2, 3 };
+            //RotatedArray(myArray, 2);
+
+            Console.WriteLine("{0}", RotatedArray(myArray, 6));
+            Console.ReadLine();
+        }
+
+        public static int RotatedArray(int[] A, int Number)
+        {
+            int start = 0;
+            int end = A.Length - 1;
+
+
+            while (start <= end)
+            {
+                int mp = ((start + end) / 2);
+                if (A[mp] == Number)
+                {
+                    return mp;
+                }
+                if (A[start] <= A[mp])
+                {
+                    if (A[start] <= Number && Number <= A[mp])
+                    {
+                        end = mp - 1;
+                    }
+
+                    else
+                    {
+                        start = mp + 1;
+                    }
+                }
+                else
+                {
+                    if (A[mp] <= Number && Number <= A[end])
+                    {
+                        start = mp + 1;
+                    }
+                    else
+                    {
+                        end = mp - 1;
+                    }
+                }
+
+            }
+            return -1;
+        }
+    }
+}
