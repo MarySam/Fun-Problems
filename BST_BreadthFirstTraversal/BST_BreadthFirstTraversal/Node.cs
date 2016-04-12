fd1f123bf4fc994d74e4e9e75a9e9e550dd849f1
Mary Sam <marysam01@hotmail.com>
2016-04-12 00:19:21 -0700

    BreadthFirstTraversal

---
+++ BST_BreadthFirstTraversal/BST_BreadthFirstTraversal/Node.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_BreadthFirstTraversal
+{
+    public class Node
+    {
+        public int value;
+        public Node left;
+        public Node right;
+    }
+
+    public class Tree
+    {
+        public Node root;
+
+        //constructor
+        public Tree()
+        {
+            root = null;
+        }
+
+        public Node ReturnRoot()
+        {
+            return root;
+        }
+
+        public void Insert(int num)
+        {
+            Node newNode = new Node();
+            newNode.value = num;
+            if (root == null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (num < current.value)
+                    {
+                        current = current.left;
+
+                        if (current == null)
+                        {
+                            parent.left = newNode;
+                            return;
+                        }
+                    }
+
+                    else
+                    {
+                        current = current.right;
+
+                        if (current == null)
+                        {
+                            parent.right = newNode;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BST_BreadthFirstTraversal/BST_BreadthFirstTraversal/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_BreadthFirstTraversal
+{
+    class Program
+    {
+        public static void breadthFirstQueue(Node root)
+        {
+            Queue<Node> q = new Queue<Node>();
+
+            if (root == null)
+                return;
+
+            q.Enqueue(root);
+
+            while (q.Count > 0)
+            {
+                Node n = q.Dequeue();
+                Console.WriteLine("{0}", n.value);
+                Console.ReadLine();
+                if (n.left != null)
+                {
+                    q.Enqueue(n.left);
+                }
+                if (n.right != null)
+                {
+                    q.Enqueue(n.right);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Tree myTree = new Tree();
+            myTree.Insert(10);
+            myTree.Insert(7);
+            myTree.Insert(5);
+            myTree.Insert(8);
+            myTree.Insert(11);
+            myTree.Insert(12);
+
+            breadthFirstQueue(myTree.root);
+
+        }
+    }
+}
+
