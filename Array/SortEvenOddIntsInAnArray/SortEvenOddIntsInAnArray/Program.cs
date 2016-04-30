7d4b28139acc5f0a27ce8cb162389f71615c466d
Mary Sam <marysam01@hotmail.com>
2016-04-30 02:41:00 -0700

    UniqueCharNoAddnlDataStructure

---
+++ Array/SortEvenOddIntsInAnArray/SortEvenOddIntsInAnArray/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortEvenOddIntsInAnArray
+{
+    class Program
+    {
+
+        public static int[] EvenOdd(int[] A)
+        {
+            int start = 0;
+            int end = A.Length - 1;
+            int temp = 0;
+            while (start < end)
+            {
+                //If the value is even, just increment the start.
+                if (A[start] % 2 == 0)
+                {
+                    start++;
+                }
+                //If the value is odd, then let's check the end of the array to see if we could swap it.
+                //If the value at the end of the array is already odd then do not swap it. Decrement until we
+                //find an even number to swap it with.
+                else if (A[end] % 2 != 0)
+                {
+                    end--;
+                }
+
+                else
+                {
+                    temp = A[start];
+                    A[start] = A[end];
+                    A[end] = temp;
+                    start++;
+                    end--;
+                }
+            }
+
+            return A;
+        }
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 2, 7, 9, 10, 12, 3 };
+            EvenOdd(A);
+            foreach (int v in A)
+            {
+                Console.WriteLine("{0}", v);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Strings/UniqueCharNoAddnlDataStruct/UniqueCharNoAddnlDataStruct/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueCharNoAddnlDataStruct
+{
+    class Program
+    {
+        public static bool IsUnique(string word)
+        {
+            bool isUnique = true;
+            for(int i=0; i<word.Length;i++)
+            {
+                for(int j=i+1; j<word.Length; j++)
+                {
+                    if(word[i]==word[j])
+                    {
+                        isUnique = false;
+                    }
+                }
+            }
+            return isUnique;
+        }
+
+        static void Main(string[] args)
+        {
+            bool result = IsUnique("taco");
+            Console.WriteLine("{0}", result);
+            Console.ReadLine();
+
+        }
+    }
+}
