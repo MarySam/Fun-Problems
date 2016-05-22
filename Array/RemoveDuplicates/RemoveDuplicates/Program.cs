d74cce23d88826b69887420b6ac7ea98c4d7f70b
Mary Sam <marysam01@hotmail.com>
2016-05-22 15:07:30 -0700

    update

---
+++ Array/RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveDuplicates
+{
+    class Program
+    {
+        public static int[] RemoveDuplicates(int[] A)
+        {
+            int j = 0;
+            for (int i= 0; i<A.Length; i++)
+            {
+                if (A[i] !=A[j])
+                {
+                    j++;
+                }
+
+                A[j] = A[i];
+            }
+
+            return A;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arrayA = new int[] { 1, 2, 3, 4, 5, 5, 6, 7, 7, 8, 8, 8, 9, 10, 10 };
+
+            RemoveDuplicates(arrayA);
+
+            foreach(int v in arrayA)
+    
+            Console.WriteLine("{0}", v);
+            Console.ReadLine();
+        }
+    }
+}
