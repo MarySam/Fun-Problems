76347a68e6d141a4e967f54ee876d3b256997840
Mary Sam <marysam01@hotmail.com>
2016-05-23 21:26:18 -0700

    EvenOddInNewArray

---
+++ Array/EvenOddInNewArray/EvenOddInNewArray/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddInNewArray
+{
+    class Program
+    {
+        public static int[] EvenOdd(int[] A)
+        {
+            int[] B = new int[A.Length];
+            int start = 0;
+            int end = B.Length - 1;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] % 2 == 0)
+                {
+                    B[start] = A[i];
+                    start++;
+                }
+
+                else
+                {
+                    B[end] = A[i];
+                    end--;
+                }
+            }
+
+            return B;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 2, 7, 5, 8, 3, 10 };
+            int[] B = new int[array.Length];
+            B = EvenOdd(array);
+
+            foreach (int v in B)
+            {
+                Console.WriteLine("{0}", v);
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- Array/RemoveDuplicates/RemoveDuplicates/Program.cs
+++ Array/RemoveDuplicates/RemoveDuplicates/Program.cs
@@ -11,9 +11,25 @@ namespace RemoveDuplicates
         public static int[] RemoveDuplicates(int[] A)
         {
             int j = 0;
-            for (int i= 0; i<A.Length; i++)
+            for (int i = 1; i < A.Length; i++)
             {
-                if (A[i] !=A[j])
+                if (A[i] != A[j])
+                {
+                    j++;
+                }
+
+                A[j] = A[i];
+            }
+
+            return A;
+        }
+
+        public static char[] RemoveCharDuplicates(char[] A)
+        {
+            int j = 0;
+            for (int i = 1; i < A.Length; i++)
+            {
+                if (A[i] != A[j])
                 {
                     j++;
                 }
@@ -27,12 +43,18 @@ namespace RemoveDuplicates
         static void Main(string[] args)
         {
             int[] arrayA = new int[] { 1, 2, 3, 4, 5, 5, 6, 7, 7, 8, 8, 8, 9, 10, 10 };
-
             RemoveDuplicates(arrayA);
 
-            foreach(int v in arrayA)
-    
+            char[] A = new char[] { 'a', 'b', 'b', 'c', 'c', 'c', };
+            RemoveCharDuplicates(A);
+
+
+            foreach (int v in arrayA)
             Console.WriteLine("{0}", v);
+            Console.ReadLine();
+
+            foreach (char c in A)
+            Console.WriteLine("{0}", c);
             Console.ReadLine();
         }
     }
