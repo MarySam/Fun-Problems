bf0e301f415056a27a82ed16f1d5949b005a209b
Mary Sam <marysam01@hotmail.com>
2016-06-03 20:39:53 -0700

    RemoveValueFromLList

---
+++ LinkedList/RemoveValueFromALinkList/RemoveValueFromALinkList/LinkedList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveValueFromALinkList
+{
+    class LinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public void AddLast(T value)
+        {
+            Node<T> newNode = new Node<T>();
+            newNode.Value = value;
+
+            if (this.Head == null)
+            {
+                this.Head = newNode;
+            }
+            else
+            {
+                Node<T> current = this.Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newNode;
+            }
+        }
+    }
+}
+
---
+++ LinkedList/RemoveValueFromALinkList/RemoveValueFromALinkList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveValueFromALinkList
+{
+    class Program
+    {
+        public static void RemoveValue(LinkedList<int> listA, int value)
+        {
+            Node<int> curr = listA.Head;
+
+            while (curr != null && curr.Next != null)
+            {
+                if (curr.Next.Value == value)
+                {
+                    curr.Next = curr.Next.Next;
+                }
+
+                else
+                {
+                    curr = curr.Next;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+    
+        }
+    }
+}
