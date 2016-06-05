0acf57707ba4c709b87a9cbe9aed55f211ba3150
Mary Sam <marysam01@hotmail.com>
2016-06-05 01:58:02 -0700

    MiddleOfLinkedList

---
+++ LinkedList/MiddleOfLinkedList/MiddleOfLinkedList/LinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleOfLinkedList
+{
+    class LinkedList <T>
+    {
+        public Node<T> Head { get; set; }
+
+        public void AddLast(T value)
+        {
+            Node<T> newNode = new Node<T>();
+            newNode.Value = value;
+
+            if (this.Head == null)
+            {
+                this.Head = newNode;
+            }
+            else
+            {
+                Node<T> current = this.Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newNode;
+            }
+        }
+    }
+}
---
+++ LinkedList/MiddleOfLinkedList/MiddleOfLinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiddleOfLinkedList
+{
+    class Program
+    {
+
+        public static Node<int> Middle (LinkedList<int> listA)
+        {
+            if (listA.Head == null)
+            {
+                return null;
+            }
+
+            Node<int> slow = listA.Head;
+            Node<int> fast = listA.Head;
+
+            while(fast!=null && fast.Next!=null)
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+            }
+
+            return slow;
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
